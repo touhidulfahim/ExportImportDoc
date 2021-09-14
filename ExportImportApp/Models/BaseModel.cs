c96727a29d675710f0043156e02ab3b79e981f12
Mohammad Touhidul Islam <muhdtohidul@gmail.com>
2021-09-14 23:13:28 +0600

    initial

---
+++ ExportImportApp/Models/BaseModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExportImportApp.Models
+{
+    public class BaseModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long SysId { get; set; }
+    }
+}
---
+++ ExportImportApp/Models/CountryModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExportImportApp.Models
+{
+    public class CountryModel:BaseModel
+    {
+        public string ShortName { get; set; }
+        public string CountryNameEn { get; set; }
+        public string CountryNameBn { get; set; }
+        public string LanguageEn { get; set; }
+        public string LanguageBn { get; set; }
+        public string CurrencyCode { get; set; }
+        public string CurrencyEn { get; set; }
+        public string CurrencyBn { get; set; }
+    }
+}
